764715c7f336d6fb00da0e604fdd0737704da750
ubahobs <ivanovs.ivans255@gmail.com>
2024-12-28 19:59:44 +0200

    Implemented base page, added some config

--- src/test/java/com/tdl/util/BasePage.java
+++ src/test/java/com/tdl/util/BasePage.java
@@ -1,4 +1,46 @@
 package com.tdl.util;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class BasePage {
+    protected WebDriver driver;
+    private final WebDriverWait wait;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10)); // 10-second timeout
+    }
+
+    protected WebElement waitForVisibility(By locator) {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    protected void click(By locator) {
+        waitForVisibility(locator).click();
+    }
+
+    protected void enterText(By locator, String text) {
+        WebElement element = waitForVisibility(locator);
+        element.clear();
+        element.sendKeys(text);
+    }
+
+    protected String getText(By locator) {
+        return waitForVisibility(locator).getText();
+    }
+
+    protected boolean isElementDisplayed(By locator) {
+        try {
+            return waitForVisibility(locator).isDisplayed();
+        } catch (Exception e) {
+            return false;
+        }
+    }
 }
+
