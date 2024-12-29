d29677c7843c5e206948806e0a90f84e1f1f13b0
ubahobs <ivanovs.ivans255@gmail.com>
2024-12-29 12:11:04 +0200

    set up test base, minor changes

--- src/test/java/com/tdl/util/TestBase.java
+++ src/test/java/com/tdl/util/TestBase.java
@@ -1,4 +1,26 @@
 package com.tdl.util;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
 public class TestBase {
+
+    @BeforeMethod
+    public void setUp() {
+        // Read browser type from the configuration file
+        String browser = ConfigUtils.getConfigProperty("browser");
+
+        // Initialize the browser
+        BrowserUtils.initializeDriver(browser);
+
+        // Open the base URL
+        String baseUrl = ConfigUtils.getConfigProperty("BASE_URL");
+        BrowserUtils.open(baseUrl);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        // Quit the browser after each test
+        BrowserUtils.quitDriver();
+    }
 }
