394a23f1b712f4c866449277623679de5427485b
ubahobs <ivanovs.ivans255@gmail.com>
2024-12-29 13:32:17 +0200

    added entrypoint for the tests

---
+++ src/test/java/com/tdl/googleMeet/Application.java
@@ -0,0 +1,12 @@
+package com.tdl.googleMeet;
+
+import com.tdl.googleMeet.pages.LandingPage;
+import com.tdl.util.BrowserUtils;
+import com.tdl.util.ConfigUtils;
+
+public class Application {
+    public static LandingPage navigateToLandingPage() {
+        BrowserUtils.open(ConfigUtils.getConfigProperty("meet.url"));
+        return new LandingPage();
+    }
+}
